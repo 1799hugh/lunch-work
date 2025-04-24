2354c1a66c49e9c5f99559899c7cccf325c1fa46
1799hugh <1799hugh@gmail.com>
2025-04-24 09:22:24 +0800

    修正 STATIC_ROOT 並配置 WhiteNoise

--- work/settings.py
+++ work/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9@64m54*jc6r0(+=)qygg&vc+t(tltipf3tf%#%bry*mpvvl8d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = False  # 確保在生產環境中關閉 DEBUG
 
 ALLOWED_HOSTS = ['web-production-1603.up.railway.app', 'localhost', '127.0.0.1']
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # 新增這一行
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # 新增這一行
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
